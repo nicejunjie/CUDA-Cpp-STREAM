16a337bd1cf09215c9be9588e416f4e582635fc1
Andreas Herten <a.herten@fz-juelich.de>
2017-02-09 13:25:10 +0100

    Add CSV output feature
    This will print
      a) an overview in comma-separated manner, or
      b) all values (min, max, avg for each STREAM mini-benchmark) in a comma-separated manner.
    
    Optionally, a header is added before the values to annotate what is written in which column.
    
    To call the new features via command line I switched from getopt() to getopt_long(), which augments the single-character command line options by multi-character aliases. Check `./stream --help` for the new parameters and what they do.

--- stream.cu
+++ stream.cu
@@ -25,7 +25,8 @@
 #include <stdio.h>
 #include <float.h>
 #include <limits.h>
-#include <unistd.h>
+// #include <unistd.h>
+#include <getopt.h>
 #include <sys/time.h>
 
 #include <sys/time.h>
@@ -46,32 +47,60 @@ static double   avgtime[4] = {0}, maxtime[4] = {0},
 void print_help()
 {
     printf(
-        "Usage: stream [-s] [-n <elements>] [-b <blocksize>]\n\n"
-        "  -s\n"
+        "Usage: stream [-s] [-c [-f]] [-n <elements>] [-b <blocksize>]\n\n"
+        "  -s, --si\n"
         "        Print results in SI units (by default IEC units are used)\n\n"
-        "  -n <elements>\n"
+        "  -c, --csv\n"
+        "        Print results CSV formatted\n\n"
+        "  -f, --full\n"
+        "        Print all results in CSV\n\n"
+        "  -t, --title\n"
+        "        Print CSV header\n\n"
+        "  -n <elements>, --nelements <element>\n"
         "        Put <elements> values in the arrays\n"
-        "        (defaults to 1<<26)\n\n"
-        "  -b <blocksize>\n"
+        "        (default: 1<<26)\n\n"
+        "  -b <blocksize>, --blocksize <blocksize>\n"
         "        Use <blocksize> as the number of threads in each block\n"
-        "        (defaults to 192)\n"
+        "        (default: 192)\n"
     );
 }
 
-void parse_options(int argc, char** argv, bool& SI, int& N, int& blockSize)
+void parse_options(int argc, char** argv, bool& SI, bool& CSV, bool& CSV_full, bool& CSV_header, int& N, int& blockSize)
 {
     // Default values
     SI = false;
+    CSV = false;
+    CSV_full = false;
+    CSV_header = false;
     N = 1<<26;
     blockSize = 192;
 
+    static struct option long_options[] = {
+            {"si",        no_argument,       0,  's' },
+            {"csv",       no_argument,       0,  'c' },
+            {"full",      no_argument,       0,  'f' },
+            {"title",     no_argument,       0,  't' },
+            {"nelements", required_argument, 0,  'n' },
+            {"blocksize", required_argument, 0,  'b' },
+            {"help",      no_argument,       0,  'h' },
+            {0,           0,                 0,  0   }
+        };
     int c;
-
-    while ((c = getopt (argc, argv, "sn:b:h")) != -1)
+    int option_index = 0;
+    while ((c = getopt_long(argc, argv, "scftn:b:h", long_options, &option_index)) != -1)
         switch (c)
         {
             case 's':
                 SI = true;
+                break;
+            case 'c':
+                CSV = true;
+                break;
+            case 'f':
+                CSV_full = true;
+                break;
+            case 't':
+                CSV_header = true;
                 break;
             case 'n':
                 N = std::atoi(optarg);
@@ -151,12 +180,14 @@ int main(int argc, char** argv)
     std::vector<std::string> label{"Copy:      ", "Scale:     ", "Add:       ", "Triad:     "};
 
     // Parse arguments
-    bool SI;
+    bool SI, CSV, CSV_full, CSV_header;
     int N, blockSize;
-    parse_options(argc, argv, SI, N, blockSize);
+    parse_options(argc, argv, SI, CSV, CSV_full, CSV_header, N, blockSize);
 
-    printf(" STREAM Benchmark implementation in CUDA\n");
-    printf(" Array size (%s precision) =%7.2f MB\n", sizeof(double)==sizeof(real)?"double":"single", double(N)*double(sizeof(real))/1.e6);
+    if (!CSV) {
+        printf("STREAM Benchmark implementation in CUDA\n");
+        printf("Array size (%s precision) = %7.2f MB\n", sizeof(double)==sizeof(real)?"double":"single", double(N)*double(sizeof(real))/1.e6);
+    }
 
     /* Allocate memory on device */
     cudaMalloc((void**)&d_a, sizeof(real)*N);
@@ -168,12 +199,14 @@ int main(int argc, char** argv)
     dim3 dimGrid(N/dimBlock.x );
     if( N % dimBlock.x != 0 ) dimGrid.x+=1;
 
-    printf(" using %d threads per block, %d blocks\n",dimBlock.x,dimGrid.x);
+    if (!CSV) {
+        printf("Using %d threads per block, %d blocks\n",dimBlock.x,dimGrid.x);
 
-    if (SI)
-        printf(" output in SI units (KB = 1000 B)\n");
-    else
-        printf(" output in IEC units (KiB = 1024 B)\n");
+        if (SI)
+            printf("Output in SI units (KB = 1000 B)\n");
+        else
+            printf("Output in IEC units (KiB = 1024 B)\n");
+    }
 
     /* Initialize memory on the device */
     set_array<real><<<dimGrid,dimBlock>>>(d_a, 2.f, N);
@@ -217,6 +250,8 @@ int main(int argc, char** argv)
             maxtime[j] = MAX(maxtime[j], times[j][k]);
         }
     }
+    for (j=0; j<4; j++)
+        avgtime[j] = avgtime[j]/(double)(NTIMES-1);
 
     double bytes[4] = {
         2 * sizeof(real) * (double)N,
@@ -227,18 +262,46 @@ int main(int argc, char** argv)
 
     // Use right units
     const double G = SI ? 1.e9 : static_cast<double>(1<<30);
+    std::string gbpersec = SI ? "GB/s" : "GiB/s";
 
-    printf("\nFunction      Rate %s  Avg time(s)  Min time(s)  Max time(s)\n",
-           SI ? "(GB/s) " : "(GiB/s)" );
-    printf("-----------------------------------------------------------------\n");
-    for (j=0; j<4; j++) {
-        avgtime[j] = avgtime[j]/(double)(NTIMES-1);
-
-        printf("%s%11.4f     %11.8f  %11.8f  %11.8f\n", label[j].c_str(),
-                bytes[j]/mintime[j] / G,
-                avgtime[j],
-                mintime[j],
-                maxtime[j]);
+    if (!CSV) {
+        printf("\nFunction      Rate %s  Avg time(s)  Min time(s)  Max time(s)\n", gbpersec.c_str() );
+        printf("-----------------------------------------------------------------\n");
+        for (j=0; j<4; j++) {
+            printf("%s%11.4f     %11.8f  %11.8f  %11.8f\n", label[j].c_str(),
+                    bytes[j]/mintime[j] / G,
+                    avgtime[j],
+                    mintime[j],
+                    maxtime[j]);
+        }
+    } else {
+        if (CSV_full) {
+            if (CSV_header)
+                printf("Copy (Max) / %s, Copy (Min) / %s, Copy (Avg) / %s, Scale (Max) / %s, Scale (Min) / %s, Scale (Avg) / %s, Add (Max) / %s, Add (Min) / %s, Add (Avg) / %s, Triad (Max) / %s, Triad (Min) / %s, Triad (Avg) / %s\n",
+                    gbpersec.c_str(), gbpersec.c_str(), gbpersec.c_str(), 
+                    gbpersec.c_str(), gbpersec.c_str(), gbpersec.c_str(), 
+                    gbpersec.c_str(), gbpersec.c_str(), gbpersec.c_str(), 
+                    gbpersec.c_str(), gbpersec.c_str(), gbpersec.c_str()
+                );
+            printf(
+                "%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f\n", 
+                bytes[0]/mintime[0] / G, bytes[0]/maxtime[0] / G, bytes[0]/(avgtime[0])/ G, 
+                bytes[1]/mintime[1] / G, bytes[1]/maxtime[1] / G, bytes[1]/(avgtime[1]) / G, 
+                bytes[2]/mintime[2] / G, bytes[2]/maxtime[2] / G, bytes[2]/(avgtime[2]) / G, 
+                bytes[3]/mintime[3] / G, bytes[3]/maxtime[3] / G, bytes[3]/(avgtime[3]) / G
+            );
+        }
+        else {
+            if (CSV_header)
+                printf("Copy (Max) / %s, Scale (Max) / %s, Add (Max) / %s, Triad (Max) / %s\n", gbpersec.c_str(), gbpersec.c_str(), gbpersec.c_str(), gbpersec.c_str());
+            printf(
+                "%0.4f,%0.4f,%0.4f,%0.4f\n", 
+                bytes[0]/mintime[0] / G, 
+                bytes[1]/mintime[1] / G, 
+                bytes[2]/mintime[2] / G, 
+                bytes[3]/mintime[3] / G
+            );
+        }
     }
 
 
